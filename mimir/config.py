66ae6ebb398f7575cc9ad1df6d4645c71e98f8ba
Meghdadk <mk2296@cam.ac.uk>
2025-02-17 11:23:07 +0000

    new config for peft base model

--- mimir/config.py
+++ mimir/config.py
@@ -119,6 +119,8 @@ class ExperimentConfig(Serializable):
     """Name for the experiment"""
     base_model: str
     """Base model name"""
+    peft_base_model: str
+    """PEFT Base model name"""
     peft_model_path: str
     """Path to target PEFT checkpoint"""
     peft_config_path: str
